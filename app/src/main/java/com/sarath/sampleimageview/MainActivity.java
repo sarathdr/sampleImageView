1f0eab86e9834f0c0a2ed2a36fd1a2b918cbc526
Sarath <sarath.dr@gmail.com>
2016-06-02 18:45:45 +0100

    Preparing branch for dev

--- app/src/main/java/com/sarath/sampleimageview/MainActivity.java
+++ app/src/main/java/com/sarath/sampleimageview/MainActivity.java
@@ -1,9 +1,7 @@
 package com.sarath.sampleimageview;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
 
 import com.sarath.widget.AlignedImageView;
@@ -14,6 +12,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         findViewById(R.id.image_view).setOnClickListener(this);
     }
